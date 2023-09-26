ce0e5c65afab19ea1b95d0c7199360b96298ed75
TzuWei <ap9940506@gmail.com>
2023-09-26 21:36:03 +0800

    refactor 316

--- src/bin/316.rs
+++ src/bin/316.rs
@@ -12,8 +12,6 @@ use std::iter::FromIterator;
 impl Solution {
     pub fn min_begin_cover_all_char(s: &[u8], mut set: HashSet<u8>) -> usize {
         let backup = set.clone();
-        // println!("{:?}", set);
-        // println!("{:?}", s);
         let n = s.len();
         let mut ans = n;
         for i in (0..n).rev() {
@@ -26,16 +24,14 @@ impl Solution {
     }
     pub fn remove_duplicate_letters(s: String) -> String {
         let n = s.len();
-        let mut l = 0;
-        let mut set = HashSet::from_iter(s.bytes());
-        let m = set.len();
+        let mut char_set = HashSet::from_iter(s.bytes());
         let mut ans = String::new();
-        for _ in 0..m {
-            let ret =
-                l + Self::min_begin_cover_all_char(&s.as_bytes()[(l as usize)..n], set.clone());
-            set.remove(&s.bytes().nth(ret).unwrap());
-            ans.push(s.chars().nth(ret).unwrap());
-            l = ret + 1;
+        let mut l = 0;
+        for _ in 0..char_set.len() {
+            l += Self::min_begin_cover_all_char(&s.as_bytes()[l..n], char_set.clone());
+            char_set.remove(&s.bytes().nth(l).unwrap());
+            ans.push(s.chars().nth(l).unwrap());
+            l += 1;
         }
         ans
     }
